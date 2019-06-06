d49db6e09b90912e1846b924372bf05d60618e24
unknown <shreyabansal577@gmail.com>
2019-06-06 12:30:44 +0700

    program for area

---
+++ area of the sqr, triangle, rect.py
@@ -0,0 +1,15 @@
+stack=[]
+stack.append("shreya")
+stack.append("rakshit")
+stack.append("prateek")
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+
+    
+    
+
+    
